d6dfca0a19193afd1d1b99ac0e7a292fe99b69c8
eito-github <eito.duties@gmail.com>
2026-05-03 14:03:01 +0900

    Portfolio セクションの実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
     <>
+      <Header />
       <Hero />
       <About />
+      <Portfolio />
+      <Footer />
     </>
   );
 }
 
 export default App;
-
-
